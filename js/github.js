028e0ad132c992a2330a8cfb9d88a3e58daf2289
Nicole Cano <arbolkiri@gmail.com>
2015-02-01 22:16:29 -0600

    now working

--- js/github.js
+++ js/github.js
@@ -3,8 +3,7 @@
     function GithubClient(token){//prototype
         this.token = token;//constructor
         this.members = [];//constructor
-        // this.repos = [];//constructor
-        
+        this.repos = [];//constructor
         var self = this;//created as a reference pointing to the function it is inside of
         
         var GithubRouter = Backbone.Router.extend({
@@ -13,7 +12,7 @@
             },
             drawUserInfo: function(username){//a value assigned to var GithubRouter; what info does this give me?
                 self.drawUser(username);
-                // self.drawRepo(username);
+                self.drawRepo(username);
 
             },
             initialize: function(){//same as above
@@ -22,39 +21,35 @@
         })
         var router = new GithubRouter();//new instance
 
-        // this.listenToEvents();
         this.draw();
-    }
+    };
 
     GithubClient.prototype = {
         URLs: {
-            members: "https://api.github.com/orgs/TIY-Houston-Front-End-Engineering/members"
-            // repos: "https://api.github.com/repos/"
-            // profileData: " "
-            // profile: function(username){
-            //     return "https://api.github.com/users/"+username
-            // },
-            // repolist: function(username){
-            //     // return "https://api.github.com/users/"+username+"/repos"
-            // }
+            members: "https://api.github.com/orgs/TIY-Houston-Front-End-Engineering/members",
+            repos: "https://api.github.com/users/"
         },
         access_token: function(){
-            return "?access_token=" + this.token
+            return "?access_token=" + this.token;
+        },
+        repo_token: function(){
+            return "/repos?access_token" + this.token;
         },
     //     *
     //      * getData
     //      * @arguments none.
     //      * @return promise
          
-        getData: function(){
+        getData: function(username){
             var x = $.Deferred();//?type of promise is this?
-                // self = this;//for reference pointing to getDatafromUser
+                self = this;//for reference pointing to getDatafromUser
 
             if(this.members.length > 0){//if I add all members, will resolve(no member name appears)
                 x.resolve(this.members);
             } else {
                 var p = $.get(this.URLs.members + this.access_token());
                 p.then(function(data){
+
                     x.resolve(data);
                     this.members = data;
                 })
@@ -66,24 +61,10 @@
         loadTemplate: function(name){
             // modify the event context, return only the data
             return $.get("./templates/"+name+".html").then(function(data) { //request for names
-                return data; 
-            })
+                return data;})
         },
 
-    //     // listenToEvents: function(){
-    //     //     var right_side = $(".github-grid > *:nth-child(2)");
-
-    //     //     right_side.on("click", "a", function(event){
-    //     //         event.preventDefault();
-    //     //         window.open(this.href,'_blank');
-    //     //     })
-
-    //     //     right_side.on("click", "input", function(event){
-    //     //         event.preventDefault();
-    //     //         this.select();
-    //     //     })
         
-
         draw: function(){//getting name info
             $.when(                       //used for more than one request at same time
                 this.getData(),//renamed above
@@ -95,58 +76,31 @@
             })
         },
 
-        // drawRepoInfo: function() {///-->STOPPED HERE; getting repo info
-        //     $.when (
-        //         this.getDatafromUser,
-        //         this.loadTemplate ("menu-item")
-        // ).then(function(repos, html) {
-
-        //     var right_column = document.querySelector(".menu-item");
-        //     right_column.innerHTML = _.template(html, {repos: repos});
-        // })
-        // }
-
-        // getProfileData: function(username){
-        //     // return $.get(this.URLs.profile(username) + this.access_token()).then(function(data){ return data });
-        // },
-
-        // getRepoList: function(username){
-        //     return $.get(this.URLs.repolist(username) + this.access_token()).then(function(data){ return data });
-        // },
-
-        // getSortedRepoList: function(username){
-        //     return this.getRepoList(username).then(function(data){
-
-        //         data.sort(function(a, b){
-
-        //             var collection = [a, b];
-        //             collection.forEach(function(v){
-        //                 if(! (v.updated_at instanceof Date) ){
-        //                     v.updated_at = new Date(v.updated_at);
-        //                 }
-        //             })
-
-        //             return a.updated_at > b.updated_at ? -1 : 1;
-        //         })
-        //         return data;
-
-        //     })
-        // },
-
         drawUser: function(username){//bringing everything together??
             // load data load template draw to screen
-            $.when(this.getData(), this.loadTemplate("right")).then(function(members, html) {
+            $.when(
+                this.getData(username), 
+                this.loadTemplate("right")
+            ).then(function(members, html) {
                 debugger;
-                arr_of_str = members.map(function(m) {
-                    return _.template(html, m)
+                var user_view = document.querySelector(".github-grid > *:nth-child(2)");
+                user_view.innerHTML = _.template(html, { members: members, user: username }); 
+
                 })
-            
-                var right_column = document.querySelector(".github-grid > *:nth-child(even)");
-                    right_column.innerHTML = arr_of_str.join()
-      })
+        },
+
+        drawRepo: function (username){
+           $.when(
+            $.get(this.URLs.repos + username + this.repo_token()),
+            this.loadTemplate("repos")
+            ).then(function(repos, html){
+                var repo_view = document.querySelector(".github-grid > *:nth-child(2)");
+                repo_view.innerHTML += _.template(html, {repos: repos[0], user: username});
+            }) 
+      
         
     }
-}
+};
 
     window.GithubClient = GithubClient;//global variable b/c of window.
 
